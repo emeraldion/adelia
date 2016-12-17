4e458a7c0167704970e32d5bd6e686405b5eab27
claudiopro <claudio.procida@gmail.com>
2016-12-17 19:19:24 +0100

    Implements getColumns API in SQLite adapter 💨

---
+++ test/adapters/sqlite.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const expect = require('chai').expect,
+  path = require('path'),
+  SQLiteAdapter = require('../../lib/adapters/sqlite');
+
+describe('SQLite adapter', function() {
+  it('is defined', function() {
+    expect(SQLiteAdapter).to.be.defined;
+  });
+
+  it('can be instantiated', function() {
+    expect(function() { new SQLiteAdapter(); }).not.to.throw;
+    const adapter = new SQLiteAdapter();
+    expect(adapter).to.not.be.null;
+  });
+
+  describe('getColumns', function() {
+    it('returns a list of columns', function(done) {
+      const adapter = new SQLiteAdapter();
+
+      adapter.connect(path.join(__dirname, '../../schemas/adelia_test.sqlite'))
+        .then(function() {
+            return adapter.getColumns('models');
+        })
+        .then(function(columns) {
+          expect(columns).to.eql(['id', 'name']);
+          done();
+        });
+    });
+  });
+});
