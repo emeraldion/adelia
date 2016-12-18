cd8af8815b4dcbb6ee679a3d5b6f0241faf27ca9
claudiopro <claudio.procida@gmail.com>
2016-12-18 13:43:29 +0100

    Updates test MySQL schema 🚧

--- schemas/adelia_test.sql
+++ schemas/adelia_test.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Dec 17, 2016 at 10:19 PM
+-- Generation Time: Dec 18, 2016 at 01:39 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -40,6 +40,44 @@ CREATE TABLE `birds` (
 --
 
 INSERT INTO `birds` (`id`, `name`, `size`, `person_id`) VALUES(1, 'Budgie', 'small', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(24) NOT NULL,
+  `year` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `year`) VALUES(1, 'Arctic Ice Meltdown', 2015);
+INSERT INTO `books` (`id`, `title`, `year`) VALUES(2, 'Saving The Planet', 2012);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books_people`
+--
+
+CREATE TABLE `books_people` (
+  `person_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `books_people`
+--
+
+INSERT INTO `books_people` (`person_id`, `book_id`) VALUES(1, 1);
+INSERT INTO `books_people` (`person_id`, `book_id`) VALUES(1, 2);
+INSERT INTO `books_people` (`person_id`, `book_id`) VALUES(2, 2);
 
 -- --------------------------------------------------------
 
@@ -108,6 +146,12 @@ ALTER TABLE `birds`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `cats`
 --
 ALTER TABLE `cats`
@@ -133,22 +177,27 @@ ALTER TABLE `people`
 -- AUTO_INCREMENT for table `birds`
 --
 ALTER TABLE `birds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `cats`
 --
 ALTER TABLE `cats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `models`
 --
 ALTER TABLE `models`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=97;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `people`
 --
 ALTER TABLE `people`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
